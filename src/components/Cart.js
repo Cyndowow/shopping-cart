09106d588932aaa433d79624fcb7dec414c5adb8
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-06-27 11:44:21 +0200

    Add Cart and cart items

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,65 @@
 import React from "react";
 import uniqid from "uniqid";
-import CardItem from "./CartItem";
+import CartItemCard from "./CartItem";
+import Button from "../utils/Button";
 
 
-const Card = () => {
 
+const Cart = ({ items, deleteCartItem, changeQty }) => {
+    
+    const updateQuantity = async (productId, quantity) => {
+        const currentCart = cart;
+
+        if(currentCart.quantity === 0) {
+            setCart(currentCart.filter((item) => item.id) !== productId)
+        } else {
+            const product = currentCart.find((item) => item.id === productId);
+            product.quantity = quantity;
+            currentCart.map((item) => (item.id === productId ? product : null));
+            setCart(currentCart)
+        }
+    }
+    
+    const sumTotal = items.map((item) => item.price * item.quantity).reduce((a, c) => a + c, 0).toFixed(2)
+    
+
+    const CartItems = items.map((cartItem) => (
+        <CartItemCard 
+            key={uniqid()}
+            id={cartItem.id}
+            title={cartItem.title}
+            price={cartItem.price}
+            image={cartItem.image}
+            quantity={cartItem.quantity}
+            changeQty={changeQty}
+            deleteCartItem={deleteCartItem}
+        />
+    ))
+
+    return(
+        <>
+        <div className="cart-wrapper">
+            <div className="cart-title">Your shopping cart!</div>
+            {items.length > 0 ? (
+                <div className="cart-card-container">
+                    <div className="cart-products">{CartItems}</div>
+                    <div className="cart-total">Total: ${sumTotal}</div>
+                    <Button 
+                        content="Checkout"
+                        className="cart-checkout-btn"
+                    />
+                </div>
+                ) : (
+                    <div className="empty-cart">
+                        <h2 className="empty-cart-text">Your cart is empty</h2>
+                    </div>    
+                    )
+                }
+        </div>
+        </>
+    )
 }
 
-export default Card;
+
+
+export default Cart;
---
+++ src/components/CartItem.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "../utils/Button";
+
+
+const CartItemCard = ({ id, title, price, image, quantity, changeQty, deleteCartItem }) => {
+    const cartItem = { id, title, price, image, quantity };
+    const product = { id, title, price, image };
+
+    const handleDelete = () => deleteCartItem(id);
+    
+
+    const sumPrice = () => {
+        return (cartItem.price * cartItem.quantity).toFixed(2);
+    }
+
+    const decrementQuant = () => changeQty(id, -1)
+
+    const incrementQuant = () => changeQty(id, 1)
+
+    return(
+        <div className="cart-item-wrapper">
+            <div className="cart-img-container">
+                <img src={image} alt={title} />
+            </div>
+            <div className="cart-info-wrapper">
+                <div className="cart-title">{title}</div>
+                <div>${sumPrice()}</div>
+                <div className="amount-changer">
+                    <Button className={"amount-plus"} onClick={incrementQuant} content="+"/>
+                    <div>{cartItem.quantity}</div>
+                    <Button className={"amount-minus"} onClick={decrementQuant} content="-"/>
+                </div>
+                <Button className={"cart-delete-item"} onClick={handleDelete} content="x" />
+            </div>
+        </div>
+    )
+}
+
+export default CartItemCard;
