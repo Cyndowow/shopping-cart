285b0026297d5157b5dc5e9974b6a14c635f9ab8
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-06-27 12:23:27 +0200

    Adjust home and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,10 @@ const Home = () => {
     return(
         <div className="home">
             <div className="home-img">
-            <img src="https://cdn.shopify.com/s/files/1/0590/6730/2066/files/illustrator.jpg?v=1678271032" alt="Image of futuristic keyboard" />
+            <img src="https://images.pexels.com/photos/210644/pexels-photo-210644.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Woman in White Button Up Top and Holding Black Android Smartphone" />
+                <p>Img src: <a>https://www.pexels.com/photo/woman-in-white-button-up-top-and-holding-black-android-smartphone-210644</a> </p>
             </div>
             <div className="home-info">
-                <p>Please check out the actual creators at </p>
                 <Link to="/shop"><button type="button">Shop now</button></Link>
             </div>
         </div>
